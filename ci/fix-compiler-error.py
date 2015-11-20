e0130e33424e6be5ef4e778008aa50b96fed1fe5
Christer van der Meeren <cmeeren@gmail.com>
2015-11-20 07:36:20 +0100

    ci testing stuff

--- ci/fix-compiler-error.py
+++ ci/fix-compiler-error.py
@@ -3,3 +3,9 @@ with open(filename) as f:
      lines = f.read().replace('raise NotImplementedError("Only MS compiler supported with gfortran on win64")', 'pass')
 with open(filename, "w") as f1:
      f1.write(lines)
+
+filename = r'C:\pythontest\Lib\distutils\cygwinccompiler.py'     
+with open(filename) as f:
+     lines = f.read().replace("            return ['msvcr100']", "            return ['msvcr100']\n        elif msc_ver == '1600':\n            return ['vcruntime140']")
+with open(filename, "w") as f1:
+     f1.write(lines)
