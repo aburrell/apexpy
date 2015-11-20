48fe779922a92ac21155368aeb044a9cd8fca0ee
Christer van der Meeren <cmeeren@gmail.com>
2015-11-20 18:04:31 +0100

    rtd testing stuff

--- src/apexpy/apex.py
+++ src/apexpy/apex.py
@@ -24,7 +24,7 @@ class Apex(object):
     '''Performs coordinate conversions
 
     Parameters
-    ----------
+    ==========
     date : float (decimal year) or instance of :class:`~datetime.date` or :class:`~datetime.datetime`
         IGRF coefficients are used in conversions. Uses current date as default.
     refh : float
@@ -33,8 +33,8 @@ class Apex(object):
         Path to custom coefficient file
 
     Methods
-    -------
-    :func:`~apexpy.Apex.convert`
+    =======
+    apexpy.Apex.convert
         High-level, general-purpose conversion between geodetic, modified apex, quasi-dipole and MLT
     :meth:`Apex.geo2apex`, :meth:`Apex.apex2geo`, :meth:`Apex.geo2qd`, :meth:`Apex.qd2geo`, \
     :meth:`Apex.apex2qd`, :meth:`Apex.qd2apex`, :meth:`Apex.mlon2mlt`, :meth:`Apex.mlt2mlon`
@@ -50,7 +50,7 @@ class Apex(object):
         Change epoch and reference height for subsequent conversions
 
     Attributes
-    ----------
+    ==========
     year : float
         Decimal year used for the IGRF model
     refh : float
