c6a366a2acc95d900e53312e9c0439527e1e2c9f
Christer van der Meeren <cmeeren@gmail.com>
2015-11-20 14:42:51 +0100

    rdt stuff

--- src/apexpy/__init__.py
+++ src/apexpy/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import
 from .apex import Apex, ApexHeightError
 from . import fortranapex, helpers
 
-__version__ = "1.0.13"
+__version__ = "0.1.0"
 
 __all__ = ['Apex', 'fortranapex', 'helpers', 'ApexHeightError']
