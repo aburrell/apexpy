1c6628254b56d3a38233647c1f395f4e68587f9c
Greg Starr <gregstarr2@gmail.com>
2021-02-22 19:37:35 -0400

    fixing 2.7 compatibility issue in apex.py

--- src/apexpy/apex.py
+++ src/apexpy/apex.py
@@ -243,7 +243,7 @@ class Apex(object):
 
         alat, alon = self._geo2apex(glat, glon, height)
 
-        alat = np.float64(alat)
+        print(alat)
         if np.any(alat == -9999):
             warnings.warn('Apex latitude set to NaN where undefined '
                           '(apex height may be < reference height)')
