6597308f1f92cf24a9751ab34a50ffa47b957432
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-05-13 08:41:57 -0400

    STY: made PEP8 improvements
    Made PEP8 improvements, and updated author list to draw from .zenodo.json.

--- docs/conf.py
+++ docs/conf.py
@@ -1,51 +1,48 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-import os
+import json
 import re
 
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.coverage',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.extlinks']
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.extlinks'
-]
+# Define common elements
 
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'Apex Python library'
-year = u'2015'
-author = u'Christer van der Meeren, Angeline G. Burrell'
-copyright = '{0}, {1}'.format(year, author)
-# Get version number from __init__.py
-here = os.path.abspath(os.path.dirname(__file__))
-regex = "(?<=__version__..\s)\S+"
-with open(os.path.join(here,'../src/apexpy/__init__.py'),'r', encoding='utf-8') as f:
-    text = f.read()
-match = re.findall(regex,text)
-version = release = match[0].strip("'")
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+project = 'ApexPy'
+year = '2021'
+zenodo = json.loads(open('../.zenodo.json').read())
+author = ' and '.join([zcreator['name'] for zcreator in zenodo['creators']])
+copyright = ', '.join([year, author])
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Get version number from __init__.py
+regex = r"(?<=__version__..\s)\S+"
+with open('../src/apexpy/__init__.py', 'r') as fin:
+    text = fin.read()
+match = re.findall(regex, text)
+version = release = match[0].strip("'")
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 
 pygments_style = 'trac'
 templates_path = ['.']
 html_use_smartypants = True
 html_last_updated_fmt = '%b %d, %Y'
 html_split_index = True
-html_sidebars = {
-   '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
-}
-html_short_title = '%s-%s' % (project, version)
-autodoc_member_order='bysource'
-napoleon_use_ivar=True
-napoleon_use_rtype=False
-napoleon_use_param=False
+html_sidebars = {'**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html']}
+html_short_title = '-'.join([project, version])
+autodoc_member_order = 'bysource'
+napoleon_use_ivar = True
+napoleon_use_rtype = False
+napoleon_use_param = False
 
 extlinks = {'doi': ('http://dx.doi.org/%s', 'doi:')}
