aa3026d6f5cfa3140e04ec6f73916a901608d4de
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-04-18 13:39:09 -0400

    STY: changed import order
    Changed the import order to fail first, if possible.

--- apexpy/__init__.py
+++ apexpy/__init__.py
@@ -1,8 +1,5 @@
 from sys import stderr
 
-from apexpy.apex import Apex, ApexHeightError
-from apexpy import helpers
-
 # Below try..catch required for autodoc to work on readthedocs
 try:
     from apexpy import fortranapex
@@ -10,6 +7,9 @@ except ImportError as ierr:
     stderr.write("".join(["fortranapex module could not be imported. ",
                           "apexpy probably won't work. Failed with error: ",
                           str(ierr)]))
+
+from apexpy.apex import Apex, ApexHeightError
+from apexpy import helpers
 
 # Define the global variables
 __version__ = "1.1.0"
