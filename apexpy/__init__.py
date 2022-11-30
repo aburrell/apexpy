d5ca2960376e72db2dbf58330b38cd182d2dc30d
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-11-30 10:20:23 -0500

    BUG: more flake8 fixes
    Changed from file-code ignore to in-line ignore.

--- apexpy/__init__.py
+++ apexpy/__init__.py
@@ -2,14 +2,14 @@ from sys import stderr
 
 # Below try..catch required for autodoc to work on readthedocs
 try:
-    from apexpy import fortranapex
+    from apexpy import fortranapex  # noqa F401
 except ImportError as ierr:
     stderr.write("".join(["fortranapex module could not be imported. ",
                           "apexpy probably won't work. Failed with error: ",
                           str(ierr)]))
 
-from apexpy.apex import Apex, ApexHeightError
-from apexpy import helpers
+from apexpy.apex import Apex, ApexHeightError  # noqa F401
+from apexpy import helpers  # noqa F401
 
 # Define the global variables
 __version__ = "1.1.0"
